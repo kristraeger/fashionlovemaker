8c7cfe52bc100534f0c0eea9168d434a25917ceb
Kristina Traeger <kristina@traeger.co>
2016-01-11 16:14:57 -0800

    animation

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@
 $(document).ready(function() {
 
 		// for all elements with .fade-slow do:
-        $(".fade-slow").mouseenter(function() {
+        $('.fade-slow').mouseenter(function() {
         	// when mouse enters element, fade to 100%
-            $(".fade-slow").fadeTo("fast",1);
+            $(this).stop().fadeTo('fast',1);
         });
         	//when mouse leaves element, fade to 50%
-        $(".fade-slow").mouseleave(function() {
-            $(".fade-slow").fadeTo("fast",0.5);
+        $('.fade-slow').mouseleave(function() {
+            $(this).stop().fadeTo('slow',0.5);
         });
 
         // when user clicks button with .chevron-down do:
-        $(".chevron-down").click(function (){
+        $('.chevron-down').click(function (){
         		//sites scrolls up so that #scrollfeatures is on top
-                $("html, body").animate({
-                    scrollTop: $("#scrollfeatures").offset().top
-                }, 1000);
+                $('html, body').animate({
+                    scrollTop: $('#scrollfeatures').offset().top
+                }, 400, 'easeInCubic');
             });
 
     });
